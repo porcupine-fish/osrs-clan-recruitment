762a8d0f6093cb3a1033e1c1e2250b950d41dc37
Porcupine Fish <porcupine-fish@outlook.com>
2026-05-17 20:36:50 +0100

    Adjust panel colours

--- src/main/java/com/clan/recruitment/ClanRecruitmentPanel.java
+++ src/main/java/com/clan/recruitment/ClanRecruitmentPanel.java
@@ -1,6 +1,7 @@
 package com.clan.recruitment;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.util.Collection;
@@ -13,6 +14,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
+import net.runelite.client.ui.ColorScheme;
 import net.runelite.client.ui.PluginPanel;
 
 public class ClanRecruitmentPanel extends PluginPanel
@@ -28,26 +30,48 @@ public class ClanRecruitmentPanel extends PluginPanel
 	public ClanRecruitmentPanel(ClanRecruitmentPlugin plugin, ClanRecruitmentConfig config)
 	{
 		super(false);
+
 		this.plugin = plugin;
 		this.config = config;
 
 		setLayout(new BorderLayout(0, 8));
 		setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		setBackground(ColorScheme.DARK_GRAY_COLOR);
 
 		enabledCheckBox.setSelected(config.enabled());
-		enabledCheckBox.addActionListener(e -> plugin.setFeatureEnabled(enabledCheckBox.isSelected()));
+		enabledCheckBox.setBackground(ColorScheme.DARK_GRAY_COLOR);
+		enabledCheckBox.setForeground(Color.WHITE);
+		enabledCheckBox.addActionListener(e ->
+			plugin.setFeatureEnabled(enabledCheckBox.isSelected()));
 
 		hiddenUsersArea.setEditable(false);
 		hiddenUsersArea.setLineWrap(true);
 		hiddenUsersArea.setWrapStyleWord(true);
 
+		hiddenUsersArea.setBackground(ColorScheme.DARKER_GRAY_COLOR);
+		hiddenUsersArea.setForeground(Color.WHITE);
+		hiddenUsersArea.setCaretColor(Color.WHITE);
+		hiddenUsersArea.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
 		JScrollPane scrollPane = new JScrollPane(hiddenUsersArea);
 		scrollPane.setPreferredSize(new Dimension(220, 220));
-		scrollPane.setBorder(BorderFactory.createTitledBorder("Hidden usernames"));
+		scrollPane.setBorder(
+			BorderFactory.createTitledBorder(
+				BorderFactory.createLineBorder(ColorScheme.MEDIUM_GRAY_COLOR),
+				"Hidden usernames"
+			)
+		);
 
+		scrollPane.getViewport().setBackground(ColorScheme.DARKER_GRAY_COLOR);
+		scrollPane.setBackground(ColorScheme.DARK_GRAY_COLOR);
+
+		clearButton.setBackground(ColorScheme.DARKER_GRAY_COLOR);
+		clearButton.setForeground(Color.WHITE);
+		clearButton.setFocusPainted(false);
 		clearButton.addActionListener(e -> plugin.clearHiddenPlayers());
 
 		JPanel top = new JPanel(new GridLayout(0, 1, 0, 6));
+		top.setBackground(ColorScheme.DARK_GRAY_COLOR);
 		top.add(enabledCheckBox);
 
 		add(top, BorderLayout.NORTH);
