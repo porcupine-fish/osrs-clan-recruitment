2b6c150f8264dfee9036620e78dfa7f7c66c68c7
Porcupine Fish <porcupine-fish@outlook.com>
2026-06-07 11:23:11 +0100

    Fix test command

--- src/test/java/com/example/ClanRecruitmentPluginTest.java
+++ src/test/java/com/example/ClanRecruitmentPluginTest.java
@@ -8,6 +8,7 @@ public class ClanRecruitmentPluginTest
 	public static void main(String[] args) throws Exception
 	{
 		ExternalPluginManager.loadBuiltin(ClanRecruitmentPlugin.class);
-		RuneLite.main(new String[]{"--developer-mode", "--disable-telemetry"});
+		// RuneLite.main(new String[]{"--developer-mode", "--disable-telemetry"});
+		RuneLite.main(args);
 	}
 }
